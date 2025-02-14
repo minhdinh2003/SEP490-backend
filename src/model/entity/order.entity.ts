cd5ba1c6a3681b2432a0435a2e03e788c2607362
minhdinh2003 <minhddhe176148@fpt.edu.vn>
2025-02-14 11:16:32 +0700

    order

---
+++ src/model/entity/order.entity.ts
@@ -0,0 +1,69 @@
+import { BaseEntity } from './base.entity';
+import { OrderStatus } from '@prisma/client';
+import { AutoMap } from '@automapper/classes';
+
+export class OrderEntity extends BaseEntity {
+  id: number; // Khóa chính tự động tăng
+
+  @AutoMap()
+  userId: number; // ID của người dùng
+
+  @AutoMap()
+  totalAmount: number; // Tổng số tiền của đơn hàng
+
+  @AutoMap()
+  status: OrderStatus; // Trạng thái đơn hàng
+
+  constructor(partial?: Partial<OrderEntity>) {
+    super();
+    if (partial) {
+      Object.assign(this, partial);
+    }
+    this.createdAt = this.createdAt || new Date();
+    this.updatedAt = new Date(); // Luôn cập nhật thời gian cập nhật
+  }
+}
+
+export class OrderItemEntity extends BaseEntity {
+  id: number; // Khóa chính tự động tăng
+
+  @AutoMap()
+  orderId: number; // ID của đơn hàng
+
+  @AutoMap()
+  productId: number; // ID của sản phẩm
+
+  @AutoMap()
+  quantity: number; // Số lượng sản phẩm trong đơn hàng
+
+  @AutoMap()
+  price: number; // Giá của sản phẩm tại thời điểm đặt hàng
+
+  constructor(partial?: Partial<OrderItemEntity>) {
+    super();
+    if (partial) {
+      Object.assign(this, partial);
+    }
+    this.createdAt = this.createdAt || new Date();
+    this.updatedAt = new Date(); // Luôn cập nhật thời gian cập nhật
+  }
+}
+
+export class OrderHistoryEntity extends BaseEntity {
+  id: number; // Khóa chính tự động tăng
+
+  @AutoMap()
+  orderId: number; // ID của đơn hàng
+
+  @AutoMap()
+  status: OrderStatus; // Trạng thái đơn hàng
+
+  constructor(partial?: Partial<OrderHistoryEntity>) {
+    super();
+    if (partial) {
+      Object.assign(this, partial);
+    }
+    this.createdAt = this.createdAt || new Date();
+    this.updatedAt = new Date(); // Luôn cập nhật thời gian cập nhật
+  }
+}
