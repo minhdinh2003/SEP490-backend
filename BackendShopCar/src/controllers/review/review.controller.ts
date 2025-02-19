96fa409449e8c0176981c40b848747a8d9856ea3
truongminh0101 <trghoangminh@gmail.com>
2025-02-19 22:13:12 +0700

    fix service and commit coding review.

---
+++ BackendShopCar/src/controllers/review/review.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { Prisma, Role } from '@prisma/client';
+import { BaseController } from 'src/base/base.controller';
+import { ReviewService } from './review.service';
+import { CoreService } from 'src/core/core.service';
+import { EntityType, ModelType } from 'src/common/reflect.metadata';
+import { ReviewEntity } from 'src/model/entity/review.entity';
+import { ReviewDto } from 'src/model/dto/review.dto';
+
+@ApiTags('Review')
+@Controller('api/review')
+export class WhitelistController extends BaseController<ReviewEntity, Prisma.ReviewCreateInput> {
+    @EntityType(ReviewEntity)
+    entity: ReviewEntity;
+
+    @ModelType(ReviewDto)
+    model: ReviewDto;
+    constructor(private reviewService: ReviewService, coreSevice: CoreService) {
+        super("review", coreSevice, reviewService);
+    }
+
+    @Post("test")
+    @ApiBody({ type: ReviewDto })
+    async apiTest(@Body() param: ReviewDto) {
+        return null;
+    }
+
+}
