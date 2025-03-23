e8d7b3e103c884b7adee684a8f50af11880c73ff
truongminh0101 <trghoangminh@gmail.com>
2025-03-24 03:05:35 +0700

    chat for task

--- Backend/src/core/core.service.ts
+++ Backend/src/core/core.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 import { MapperService } from 'src/common/services/mapper.service';
 import { EmailService } from '../common/services/email.service';
 import { HttpContextService } from 'src/common/services/http-context.service';
+import { UploadService } from 'src/common/services/upload.service';
+import { NotificationService } from 'src/common/services/notification.service';
 
 @Injectable()
 export class CoreService {
@@ -10,6 +12,8 @@ export class CoreService {
     private readonly mapper: MapperService,
     private readonly emailService: EmailService,
     private readonly authService: HttpContextService,
+    private readonly uploadService: UploadService,
+    private readonly notificationService: NotificationService
   ) {
 
   }
@@ -23,6 +27,14 @@ export class CoreService {
 
   getAuthService(): HttpContextService {
     return this.authService;
+  }
+
+  getUploadFileService(): UploadService {
+    return this.uploadService
+  }
+
+  getNotificationService(): NotificationService {
+    return this.notificationService;
   }
 
 }
