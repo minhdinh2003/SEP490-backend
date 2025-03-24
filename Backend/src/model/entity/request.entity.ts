0838ef23688bd75876459fe7fb1097557925d634
truongminh0101 <trghoangminh@gmail.com>
2025-03-24 13:31:12 +0700

    Request dto entity

---
+++ Backend/src/model/entity/request.entity.ts
@@ -0,0 +1,48 @@
+import { BaseEntity } from "./base.entity";
+import { UserEntity } from "./user.entity";
+import { RequestStatus } from "@prisma/client";
+import { AutoMap } from '@automapper/classes';
+
+export class RequestEntity extends BaseEntity {
+    @AutoMap()
+    id: number;
+
+    @AutoMap()
+    userId: number;
+
+    user: UserEntity;
+
+    @AutoMap()
+    description: string;
+
+    @AutoMap()
+    status: RequestStatus;
+
+    @AutoMap()
+    price?: number;
+
+    @AutoMap()
+    approvedId?: number;
+    
+    approver?: UserEntity;
+
+    @AutoMap()
+    completedAt?: Date;
+
+    @AutoMap()
+    images: any; // Thay đổi thành kiểu dữ liệu cụ thể nếu có cấu trúc JSON xác định
+
+    @AutoMap()
+    isUserConfirm: boolean;
+
+    @AutoMap()
+    type: string;
+
+    @AutoMap()
+    reasonReject: string;
+
+    constructor(partial?: Partial<RequestEntity>) {
+        super();
+        Object.assign(this, partial);
+    }
+}
