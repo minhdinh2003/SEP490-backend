e7fe32591fe564c4e88c4bfddf4b70792535ea8c
minhdinh2003 <minhddhe176148@fpt.edu.vn>
2025-04-16 14:25:48 +0700

    confirm account through email

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,58 +1,61 @@
-import { Controller, Post, Body, UseGuards, Req } from '@nestjs/common';
-import { RegisterDto } from '../../model/dto/auth.dto';
-import { AuthService } from './auth.service';
-import { ServiceResponse } from 'src/model/response/service.response';
-import { LoginRequest } from 'src/model/request/login.request';
-import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
-import { ChangePasswordRequest, ForgotPassswordRequest, ResetPasswordRequest } from 'src/model/request/forgotPassword.request';
-import { AuthGuard } from 'src/core/auth.guard';
+import { Controller, Post, Body, UseGuards, Req, Query, Get } from '@nestjs/common';
 import { Request } from 'express';
+import { ApiBearerAuth, ApiBody, ApiTags, ApiQuery } from '@nestjs/swagger';
+
+import { AuthService } from './auth.service';
+import { AuthGuard } from 'src/core/auth.guard';
+
+import { RegisterDto } from '../../model/dto/auth.dto';
+import { LoginRequest } from 'src/model/request/login.request';
+import { ChangePasswordRequest, ForgotPassswordRequest, ResetPasswordRequest } from 'src/model/request/forgotPassword.request';
+import { ServiceResponse } from 'src/model/response/service.response';
 
 @ApiTags('Auth')
 @ApiBearerAuth()
 @Controller('api/auth')
 export class AuthController {
-
-    constructor(private authService: AuthService){}
+    constructor(private readonly authService: AuthService) {}
 
     @Post('register')
-    @ApiBody({type: RegisterDto})
-    register(@Body() body: RegisterDto): Promise<ServiceResponse>{
+    @ApiBody({ type: RegisterDto })
+    register(@Body() body: RegisterDto): Promise<ServiceResponse> {
         return this.authService.register(body);
     }
 
     @Post('login')
-    @ApiBody({type: LoginRequest})
-    login(@Body() body: LoginRequest): Promise<ServiceResponse>{
+    @ApiBody({ type: LoginRequest })
+    login(@Body() body: LoginRequest): Promise<ServiceResponse> {
         return this.authService.login(body);
     }
 
-    @Post('forgotPassword')
-    @ApiBody({type: ForgotPassswordRequest})
-    forgotPassword(@Body() body: ForgotPassswordRequest): Promise<ServiceResponse>{
+    
+    @Post('forgot-password')
+    @ApiBody({ type: ForgotPassswordRequest })
+    forgotPassword(@Body() body: ForgotPassswordRequest): Promise<ServiceResponse> {
         return this.authService.forgotPassword(body);
     }
 
-    @Post('resetPassword')
-    @ApiBody({type: ResetPasswordRequest})
-    resetPassword(@Body() body: ResetPasswordRequest): Promise<ServiceResponse>{
+    @Post('reset-password')
+    @ApiBody({ type: ResetPasswordRequest })
+    resetPassword(@Body() body: ResetPasswordRequest): Promise<ServiceResponse> {
         return this.authService.resetPassword(body);
     }
 
-    @UseGuards(AuthGuard) // Bảo vệ route bằng JWT Guard
+    @UseGuards(AuthGuard)
     @Post('logout')
-    logout(@Req() request: Request): any {
-        const token = request.headers['authorization']; // Lấy token từ request headers
+    logout(@Req() request: Request): Promise<ServiceResponse> {
+        const token = request.headers['authorization'];
         if (token) {
             this.authService.addTokenToBlacklist(token);
         }
-
-        return { message: 'Logout successful' };
+        return Promise.resolve(ServiceResponse.onSuccess({ message: 'Logout successful' }));
     }
 
-    @UseGuards(AuthGuard) // Bảo vệ route bằng JWT Guard
+    @UseGuards(AuthGuard)
     @Post('change-password')
-    changePassword(@Body() request: ChangePasswordRequest): any {
+    @ApiBody({ type: ChangePasswordRequest })
+    changePassword(@Body() request: ChangePasswordRequest): Promise<ServiceResponse> {
         return this.authService.changePassword(request);
     }
+
 }
